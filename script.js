d85a8756a6fab7e9e3054661afde1dde10566e8e
Martina Vargas <martinavargass04@gmail.com>
2023-04-11 17:12:16 -0300

    modifiqué los archivos para darle funcionalidad a la página.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const usernames = ['martinavargas', 'florenciavivas', 'SantiagoLongo2'];
+const usernames = ['martinavargas', 'florenciavivas', 'SantiagoLongo2', 'TobiasGonzalezz'];
 
-  $(document).ready(function(){
-    $.each(usernames, function(index, username) {
-      $.ajax({
-        url: `https://api.github.com/users/${username}`,
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-          console.log(data);
-          $("#info").append("followers: " + data.followers + " Nombre: " + data.name + " Ubicacion: " + data.location +" Imagen: " + `<img src="${data.avatar_url}" alt="">` + "<br/>");
-        },
-        error: function() {
-          console.error();
-        }
-      });
+$(document).ready(function () {
+  $.each(usernames, function (index, username) {
+    let $userDiv = $("<div class='user-info'></div>");
+    $.ajax({
+      url: `https://api.github.com/users/${username}`,
+      type: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        console.log(data);
+        $userDiv.append(`<img id="imagen" src="${data.avatar_url}" alt="">` + "<br/>" + `<p id="nombre">${data.name}</p>` + `<p id="biografia">${data.bio}</p>` + `<p id="ubicacion">${data.location}</p>` + `<a href='${data.html_url}'><button id="contactame" >Contáctame</button></a>`);
+        $("#info").append($userDiv)
+      },
+      error: function () {
+        console.error();
+      }
     });
   });
+});
